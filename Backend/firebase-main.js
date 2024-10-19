66cb7a7000ad5f0edcf624ce11508b97d6e9a4ce
anithjoy <anithjoy1@gmail.com>
2024-10-19 19:18:55 -0400

    just commit - 10/19/2024 07:18

--- Backend/firebase-main.js
+++ Backend/firebase-main.js
@@ -1,1 +1,23 @@
 require("dotenv").config();
+
+// import { GoogleAuthProvider } from "firebase/auth";
+
+// const provider = new GoogleAuthProvider();
+
+/* By reading document */
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// changes done
